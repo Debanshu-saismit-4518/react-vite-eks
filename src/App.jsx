09ea05071768552cbc5ce4e077131a42eb5c29fc
root <root@ip-172-31-3-106.ap-south-1.compute.internal>
2026-07-18 07:43:38 +0000

    Initial React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>React Vite Application</h1>
+      <h2>CI/CD Pipeline using Jenkins and Amazon EKS</h2>
+      <p>Internship Project</p>
+    </>
+  )
+}
+
+export default App
